45df7088fd0639d0c36ddbfc82fbefe95de38b54
N0tPVan5 <arel2226a@gmail.com>
2023-03-29 18:11:53 +0300

    Added callbackGetAlbumData() at DatabaseAccess.cpp (another support function)

--- Gallery/DatabaseAccess.cpp
+++ Gallery/DatabaseAccess.cpp
@@ -2,13 +2,43 @@
 
 /*
 this function returns the first argument a sql table is giving.
-input: pointer to data from outside, amount of columns, data of columns and name of columns
+input: pointer to string from outside, amount of columns, data of columns and name of columns
 output: 0.
 */
 int returnFirstArgument(void* data, int argc, char** argv, char** azColName)
 {
 	std::string* strData = static_cast<std::string*>(data);
 	*strData = argv[0];
+	return 0;
+}
+
+/*
+this function sets data from the database on an Album object.
+input: pointer to Album object, amount of columns, data of columns and name of columns.
+output: 0.
+*/
+int callbackGetAlbumData(void* data, int argc, char** argv, char** azColName)
+{
+	Album* myData = static_cast<Album*>(data);
+
+	for (int i = 0; i < argc; i++)
+	{
+		if (argv[i] != nullptr)
+		{
+			if (strcmp(azColName[i], "USER_ID") == 0)
+			{
+				myData->setOwner(std::atoi(argv[i]));
+			}
+			else if (strcmp(azColName[i], "NAME") == 0)
+			{
+				myData->setName(std::string(argv[i]));
+			}
+			else if (strcmp(azColName[i], "CREATION_DATE") == 0)
+			{
+				myData->setCreationDate(std::string(argv[i]));
+			}
+		}
+	}
 	return 0;
 }
 
