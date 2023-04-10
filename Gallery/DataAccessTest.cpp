eacb9bce5b5a4fe7fa55b44b9d636b0467deae8e
N0tPVan5 <arel2226a@gmail.com>
2023-04-10 04:02:23 +0300

    Wrote addData() at DataAccessTest.cpp. Changed id to auto increment when creating tables at code.

--- Gallery/DataAccessTest.cpp
+++ Gallery/DataAccessTest.cpp
@@ -16,10 +16,10 @@ void DataAccessTest::buildTables()
 		return;
 	}
 
-	const char* sqlStatement = "CREATE TABLE IF NOT EXISTS Users(ID int PRIMARY KEY, NAME TEXT);"
-		"CREATE TABLE IF NOT EXISTS Albums(ID int NOT NULL, NAME TEXT, CREATION_DATE TEXT, USER_ID int, FOREIGN KEY(USER_ID) REFERENCES USERS(ID));"
-		"CREATE TABLE IF NOT EXISTS Pictures(ID int PRIMARY KEY, NAME TEXT, CREATION_DATE TEXT, LOCATION TEXT, ALBUM_ID int, FOREIGN KEY(ALBUM_ID) REFERENCES ALBUM(ID));"
-		"CREATE TABLE IF NOT EXISTS TAGS(ID int PRIMARY KEY, PICTURE_ID int, USER_ID int, FOREIGN KEY(PICTURE_ID) REFERENCES PICTURES(ID), FOREIGN KEY(USER_ID) REFERENCES USERS(ID));";
+	const char* sqlStatement = "CREATE TABLE IF NOT EXISTS Users(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT);"
+		"CREATE TABLE IF NOT EXISTS Albums(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, CREATION_DATE TEXT, USER_ID int, FOREIGN KEY(USER_ID) REFERENCES USERS(ID));"
+		"CREATE TABLE IF NOT EXISTS Pictures(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, CREATION_DATE TEXT, LOCATION TEXT, ALBUM_ID int, FOREIGN KEY(ALBUM_ID) REFERENCES ALBUM(ID));"
+		"CREATE TABLE IF NOT EXISTS TAGS(ID INTEGER PRIMARY KEY AUTOINCREMENT, PICTURE_ID int, USER_ID int, FOREIGN KEY(PICTURE_ID) REFERENCES PICTURES(ID), FOREIGN KEY(USER_ID) REFERENCES USERS(ID));";
 	char** errMessage = nullptr;
 
 	int res = sqlite3_exec(this->_db, sqlStatement, nullptr, nullptr, errMessage);
@@ -33,11 +33,28 @@ void DataAccessTest::buildTables()
 	this->_db = nullptr;
 }
 
+/*
+this function adds 3 users to the database.
+input: none.
+output: none.
+*/
 void DataAccessTest::addData()
 {
 	if (!openDb())
 	{
 		return;
+	}
+
+	const char* sqlStatement = "INSERT INTO USERS(NAME,ID) VALUES ('Harel');"
+							   "INSERT INTO USERS(NAME,ID) VALUES ('Guy');"
+							   "INSERT INTO USERS(NAME,ID) VALUES ('Dvir');";
+	char** errMessage = nullptr;
+
+	int res = sqlite3_exec(this->_db, sqlStatement, nullptr, nullptr, errMessage);
+
+	if (res != SQLITE_OK)
+	{
+		std::cout << errMessage;
 	}
 
 	sqlite3_close(this->_db);
@@ -66,6 +83,11 @@ void DataAccessTest::deleteData()
 	this->_db = nullptr;
 }
 
+/*
+this function opens the database at _db field and creates it if necessary.
+input: none.
+output: true if the database opened successfully and otherwise false.
+*/
 bool DataAccessTest::openDb()
 {
 	std::string dbFileName = "Gallery.sqlite";
